41a7864f8f27096e7cea4aa76f093af308fea960
Marco <marco.bellocchi94@gmail.com>
2017-12-06 16:24:47 +0100

    Create Notifications.cs
    Create Notification Hub Instance for the app back end.

---
+++ Notifications.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Aidvanced Srl. All rights reserved.
+using Microsoft.Azure.NotificationHubs;
+public class Notifications
+{
+    public static Notifications Instance = new Notifications();
+
+    public NotificationHubClient Hub { get; set; }
+
+    private Notifications()
+    {
+        Hub = NotificationHubClient.CreateClientFromConnectionString("YOUR_APP_BACKPOINT");
+    }
+}
